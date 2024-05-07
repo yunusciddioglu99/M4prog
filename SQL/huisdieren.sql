871f4d16978c6c91e9ab3762343b41e124e9f37b
Yunus <38208@ma-web.nl>
2024-05-07 12:07:30 +0200

    les1_opdracht3

---
+++ SQL/huisdieren.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Generation Time: May 07, 2024 at 10:04 AM
+-- Server version: 11.3.2-MariaDB-1:11.3.2+maria~ubu2204
+-- PHP Version: 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m4prog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `huisdieren`
+--
+
+CREATE TABLE `huisdieren` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(150) NOT NULL,
+  `eigenaar` varchar(150) NOT NULL,
+  `typedier` varchar(80) NOT NULL,
+  `leeftijd` int(11) NOT NULL,
+  `geboortedatum` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `huisdieren`
+--
+ALTER TABLE `huisdieren`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `huisdieren`
+--
+ALTER TABLE `huisdieren`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
