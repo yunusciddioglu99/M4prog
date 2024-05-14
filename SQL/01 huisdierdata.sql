cfe73efce020023ea0247596a6a9ba6b7d00b853
Yunus <38208@ma-web.nl>
2024-05-14 12:23:32 +0200

    Les2_opdracht1

---
+++ SQL/01 huisdierdata.sql
@@ -0,0 +1,17 @@
+INSERT INTO `huisdierendata` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES (NULL, 'Jimmy', 'Plop', 'Kat', '6', '2018-05-16');
+
+
+
+INSERT INTO `huisdierendata` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES (NULL, 'Bob', 'Qwerty', 'Hond', '9', '2015-08-27');
+
+
+INSERT INTO `huisdierendata` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES 
+(NULL, 'Jimmy', 'Plop', 'Kat', '6', '2018-05-16'),
+(NULL, 'Bob', 'Qwerty', 'Hond', '9', '2015-08-27'),
+(NULL, 'Sal', 'Godrick the Grafted', 'Vogel', '2', '2022-02-02'),
+(NULL, 'Mike', 'Lilly', 'Paard', '7', '2017-11-21'),
+(NULL, 'Ross', 'Harvey', 'Vis', '1', '2023-01-01'),
+(NULL, 'May', 'Specter', 'Konijn', '3', '2021-09-30');
