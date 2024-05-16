cd2c1e353282f1fed1739e64719885f3db758ca6
Yunus <38208@ma-web.nl>
2024-05-16 14:17:52 +0200

    les3_opdracht1

---
+++ SQL/03 rekenen.sql
@@ -0,0 +1,7 @@
+SELECT SUM(leeftijd) FROM `huisdierendata` 
+
+SELECT AVG(leeftijd) FROM `huisdierendata` 
+
+SELECT SUM(leeftijd),AVG(leeftijd) FROM `huisdierendata` 
+
+SELECT SUM(leeftijd) as gemiddeldeleeftijd FROM `huisdierendata` 
